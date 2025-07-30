ddae846ed08788959dbcb142479f034e06d1010e
Jerry Sievert <code@legitimatesounding.com>
2025-07-30 15:27:27 -0700

    Updated to 0.2.0 release
    Allow `assert` to be passed as the test arguments.
    Add `assert.ok`

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # ST (Stupid/Simple Test)
 
-A dumb test framework for Javascript for ES Modules.
+A dumb test framework for JavaScript for ES Modules.
 
 ## Quickstart in Node:
 
 `test.mjs`:
 
 ```
-const { assert, context, dotreporter, test } = import 'st';
+const { context, dotreporter, test } = import 'st';
 
-await test('1 + 1 = 2', async () => {
+await test('1 + 1 = 2', async (t) => {
   const value = 1 + 1;
 
-  assert.eq(value, 2, 'answer should be 2');
+  t.assert.eq(value, 2, 'answer should be 2');
 });
 
 dotreporter(context);
@@ -27,10 +27,10 @@ $ node test.mjs
 `test.mjs`:
 
 ```
-const { assert, test } = import 'st';
+const { test } = import 'st';
 
-test('normal test', () => {
-  assert.eq(1, 1, '1 should equal 1');
+test('normal test', (t) => {
+  t.assert.eq(1, 1, '1 should equal 1');
 });
 ```
 
--- tests/assert.test.mjs
+++ tests/assert.test.mjs
@@ -1,36 +1,36 @@
-import { assert, context, test } from '../index.mjs';
+import { test } from '../index.mjs';
 
-await test('assert.eq', async () => {
+await test('assert.eq', async (t) => {
   const value = 1 + 1;
 
-  assert.eq(value, 2, 'two numbers should be equal');
-  assert.eq(String(value), '2', 'two strings should be equal');
+  t.assert.eq(value, 2, 'two numbers should be equal');
+  t.assert.eq(String(value), '2', 'two strings should be equal');
 });
 
-test('assert.gte', async () => {
-  assert.gte(1, 0, 'larger numbers should pass');
-  assert.gte(1, 1, 'equal numbers should pass');
+test('assert.gte', async (t) => {
+  t.assert.gte(1, 0, 'larger numbers should pass');
+  t.assert.gte(1, 1, 'equal numbers should pass');
 
-  assert.gte('a', 'a', 'equal strings should pass');
-  assert.gte('b', 'a', 'larger strings should pass');
+  t.assert.gte('a', 'a', 'equal strings should pass');
+  t.assert.gte('b', 'a', 'larger strings should pass');
 });
 
-test('assert.lte', async () => {
-  assert.lte(0, 1, 'smaller numbers should pass');
-  assert.lte(1, 1, 'equal numbers should pass');
+test('assert.lte', async (t) => {
+  t.assert.lte(0, 1, 'smaller numbers should pass');
+  t.assert.lte(1, 1, 'equal numbers should pass');
 
-  assert.lte('a', 'a', 'equal strings should pass');
-  assert.lte('a', 'b', 'smaller strings should pass');
+  t.assert.lte('a', 'a', 'equal strings should pass');
+  t.assert.lte('a', 'b', 'smaller strings should pass');
 });
 
-test('assert.gt', async () => {
-  assert.gt(1, 0, 'larger numbers should pass');
+test('assert.gt', async (t) => {
+  t.assert.gt(1, 0, 'larger numbers should pass');
 
-  assert.gt('b', 'a', 'larger strings should pass');
+  t.assert.gt('b', 'a', 'larger strings should pass');
 });
 
-test('assert.lt', async () => {
-  assert.lt(1, 2, 'smaller numbers should pass');
+test('assert.lt', async (t) => {
+  t.assert.lt(1, 2, 'smaller numbers should pass');
 
-  assert.lt('a', 'b', 'smaller strings should pass');
+  t.assert.lt('a', 'b', 'smaller strings should pass');
 });
